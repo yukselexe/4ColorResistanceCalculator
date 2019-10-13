df9acea8234a02aca1a19cefdf41cf966896ea07
Yüksel Ant Özdemir <48071814+yukselexe@users.noreply.github.com>
2019-10-13 23:51:01 +0300

    Add files via upload

---
+++ DirencHesaplama_-17193/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace DirencHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2"),
+            resources.GetString("comboBox2.Items3"),
+            resources.GetString("comboBox2.Items4"),
+            resources.GetString("comboBox2.Items5"),
+            resources.GetString("comboBox2.Items6"),
+            resources.GetString("comboBox2.Items7"),
+            resources.GetString("comboBox2.Items8"),
+            resources.GetString("comboBox2.Items9")});
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            resources.ApplyResources(this.comboBox3, "comboBox3");
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            resources.GetString("comboBox3.Items"),
+            resources.GetString("comboBox3.Items1"),
+            resources.GetString("comboBox3.Items2"),
+            resources.GetString("comboBox3.Items3"),
+            resources.GetString("comboBox3.Items4"),
+            resources.GetString("comboBox3.Items5"),
+            resources.GetString("comboBox3.Items6"),
+            resources.GetString("comboBox3.Items7"),
+            resources.GetString("comboBox3.Items8"),
+            resources.GetString("comboBox3.Items9")});
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            resources.ApplyResources(this.comboBox4, "comboBox4");
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            resources.GetString("comboBox4.Items"),
+            resources.GetString("comboBox4.Items1"),
+            resources.GetString("comboBox4.Items2"),
+            resources.GetString("comboBox4.Items3")});
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label6.Name = "label6";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label5.Name = "label5";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+        public System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Label label7;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DirencHesaplama_-17193/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirencHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+
+
+            double renk1, renk2, renk3, renk4, sonuc;
+            
+
+                renk1 = comboBox1.SelectedIndex;
+                renk2 = comboBox2.SelectedIndex;
+                renk3 = comboBox3.SelectedIndex;
+                renk4 = comboBox4.SelectedIndex;
+                string top = renk1 + renk2.ToString();
+                sonuc = (Convert.ToDouble(top) * Math.Pow(10, renk3));
+
+            
+
+
+            if (sonuc < 1000)
+                 label5.Text = sonuc + " Ohm".ToString();
+                else if (sonuc < 1000000)
+                    label5.Text = (sonuc / 1000) + " Kilo Ohm".ToString();
+                else if (sonuc < 1000000000)
+                    label5.Text = (sonuc / 1000000) + " Mega Ohm".ToString();
+                else if (sonuc < 1000000000000)
+                    label5.Text = (sonuc / 1000000000) + " Giga Ohm".ToString();
+                else
+                    label5.Text = "Hata";
+
+                if (renk4 == 0)
+                    label6.Text = " %1";
+                else if (renk4 == 1)
+                    label6.Text = " %2";
+                else if (renk4 == 2)
+                    label6.Text = " %5";
+                else
+                    label6.Text = " %10";
+            
+        }
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult x = MessageBox.Show("Direnç Hesaplama uygulamasından çıkmak ister misiniz ?", "Uyarı !", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (x == DialogResult.Yes)
+            {
+                
+               Environment.Exit(0);
+
+            }
+            else if (x == DialogResult.No)
+            {
+                
+                e.Cancel = true; 
+            }
+            
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+
+
+            progressBar1.Increment(6);
+
+            label9.Text = "%" + progressBar1.Value.ToString();
+            
+            if (progressBar1.Value == 100)
+            {
+               
+                timer1.Stop();
+                MessageBox.Show("Hesaplama Tamamlandı!");
+
+
+                label5.Visible = true;
+                label6.Visible = true;
+                label7.Visible = true;
+                label8.Visible = true;
+
+                progressBar1.Visible = false;
+                label9.Visible = false;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoşgeldiniz. Umarım program işinize yarar :=)");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            if (comboBox1.Text == "Siyah")
+            {
+                button2.BackColor = Color.Black;
+            }
+            else if (comboBox1.Text == "Kahverengi")
+            {
+                button2.BackColor = Color.SaddleBrown;
+            }
+            else if (comboBox1.Text == "Kırmızı")
+            {
+                button2.BackColor = Color.Red;
+            }
+            else if (comboBox1.Text == "Turuncu")
+            {
+                button2.BackColor = Color.Orange;
+            }
+            else if (comboBox1.Text == "Sarı")
+            {
+                button2.BackColor = Color.Yellow;
+            }
+            else if (comboBox1.Text == "Yeşil")
+            {
+                button2.BackColor = Color.Green;
+            }
+            else if (comboBox1.Text == "Mavi")
+            {
+                button2.BackColor = Color.Blue;
+            }
+            else if (comboBox1.Text == "Mor")
+            {
+                button2.BackColor = Color.Purple;
+            }
+            else if (comboBox1.Text == "Gri")
+            {
+                button2.BackColor = Color.Gray;
+            }
+            else if (comboBox1.Text == "Beyaz")
+            {
+                button2.BackColor = Color.White;
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Siyah")
+            {
+                button3.BackColor = Color.Black;
+            }
+            else if (comboBox2.Text == "Kahverengi")
+            {
+                button3.BackColor = Color.SaddleBrown;
+            }
+            else if (comboBox2.Text == "Kırmızı")
+            {
+                button3.BackColor = Color.Red;
+            }
+            else if (comboBox2.Text == "Turuncu")
+            {
+                button3.BackColor = Color.Orange;
+            }
+            else if (comboBox2.Text == "Sarı")
+            {
+                button3.BackColor = Color.Yellow;
+            }
+            else if (comboBox2.Text == "Yeşil")
+            {
+                button3.BackColor = Color.Green;
+            }
+            else if (comboBox2.Text == "Mavi")
+            {
+                button3.BackColor = Color.Blue;
+            }
+            else if (comboBox2.Text == "Mor")
+            {
+                button3.BackColor = Color.Purple;
+            }
+            else if (comboBox2.Text == "Gri")
+            {
+                button3.BackColor = Color.Gray;
+            }
+            else if (comboBox2.Text == "Beyaz")
+            {
+                button3.BackColor = Color.White;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text == "Siyah")
+            {
+                button4.BackColor = Color.Black;
+            }
+            else if (comboBox3.Text == "Kahverengi")
+            {
+                button4.BackColor = Color.SaddleBrown;
+            }
+            else if (comboBox3.Text == "Kırmızı")
+            {
+                button4.BackColor = Color.Red;
+            }
+            else if (comboBox3.Text == "Turuncu")
+            {
+                button4.BackColor = Color.Orange;
+            }
+            else if (comboBox3.Text == "Sarı")
+            {
+                button4.BackColor = Color.Yellow;
+            }
+            else if (comboBox3.Text == "Yeşil")
+            {
+                button4.BackColor = Color.Green;
+            }
+            else if (comboBox3.Text == "Mavi")
+            {
+                button4.BackColor = Color.Blue;
+            }
+            else if (comboBox3.Text == "Mor")
+            {
+                button4.BackColor = Color.Purple;
+            }
+            else if (comboBox3.Text == "Gri")
+            {
+                button4.BackColor = Color.Gray;
+            }
+            else if (comboBox3.Text == "Beyaz")
+            {
+                button4.BackColor = Color.White;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.Text == "Kahverengi")
+            {
+                button5.BackColor = Color.SaddleBrown;
+            }
+            else if (comboBox4.Text == "Kırmızı")
+            {
+                button5.BackColor = Color.Red;
+            }
+            else if (comboBox4.Text == "Altın")
+            {
+                button5.BackColor = Color.Gold;
+            }
+            else if (comboBox4.Text == "Gümüş")
+            {
+                button5.BackColor = Color.Silver;
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+    
+
